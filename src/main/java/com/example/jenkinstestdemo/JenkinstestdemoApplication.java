f37885fcfb80ed6040929213cb3463948fb040c3
shravan <rnshrai@gmail.com>
2025-06-22 20:00:31 +0530

    changes

--- src/main/java/com/example/jenkinstestdemo/JenkinstestdemoApplication.java
+++ src/main/java/com/example/jenkinstestdemo/JenkinstestdemoApplication.java
@@ -8,6 +8,7 @@ public class JenkinstestdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinstestdemoApplication.class, args);
+		System.out.println("hihi");
 	}
 //
 	//
